1496a73a9b062f556386438fa51e8a7f703527b8
S. Tan <wanderlust@thosetans.com>
2015-08-10 16:42:14 +0000

    adding create joke function

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -7,6 +7,18 @@ class JokesController < ApplicationController
     @joke = Joke.new
   end
 
+  def create
+    Joke.create(joke_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def joke_params
+    params.require(:joke).permit(:quip, :comedian)
+  end
+
 
 
 
